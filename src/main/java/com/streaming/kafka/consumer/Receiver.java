e30e82f8368a9d5ab75f0d1c6231a144ed4c263b
PAYPALCORP\rajvenkatasubram <rajvenkatasubram@paypal.com>
2018-11-24 16:57:00 +0530

    Fixing thread safe logic for pause/resume consumer.

--- src/main/java/com/streaming/kafka/consumer/Receiver.java
+++ src/main/java/com/streaming/kafka/consumer/Receiver.java
@@ -15,6 +15,7 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.KafkaListenerContainerFactory;
@@ -26,24 +27,39 @@ import org.springframework.kafka.event.ConsumerPausedEvent;
 import org.springframework.kafka.event.KafkaEvent;
 import org.springframework.kafka.event.ListenerContainerIdleEvent;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.listener.KafkaMessageListenerContainer;
 import org.springframework.kafka.listener.MessageListenerContainer;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Service;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 @Service
-public class Receiver implements ApplicationListener<ListenerContainerIdleEvent>{
+@EnableKafka
+public class Receiver /*implements ApplicationListener<ListenerContainerIdleEvent>*/ implements ApplicationListener<KafkaEvent> {
 
     private static final LogManager1 logger = new LogManager1(Receiver.class);
 
-    int recordCountPerIteration=0;
-
+    int recordCountPerIteration = 0;
     @Autowired
-    private KafkaListenerEndpointRegistry registry;
+    public KafkaListenerEndpointRegistry registry;
 
-    @Override
+    DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+    public void onApplicationEvent(KafkaEvent event) {
+        if (registry.getListenerContainer("pause.resume").isContainerPaused()) {
+            System.out.println("Resuming Idle Consumer : "+dateFormat.format( new Date()));
+            System.out.println("Container state :"+registry.getListenerContainer("pause.resume").isPauseRequested());
+            registry.getListenerContainer("pause.resume").resume();
+        }
+    }
+
+
+   /*
+   is not thread safe.
+   @Override
     public void onApplicationEvent(ListenerContainerIdleEvent event) {
         Set<TopicPartition> paused = event.getConsumer().paused();
 
@@ -55,19 +71,21 @@ public class Receiver implements ApplicationListener<ListenerContainerIdleEvent>
         }else{
             System.out.println("No paused Partitions.Container idle.");
         }
+        //System.out.printf("%s-%d[%d] \"%s\"\n", topics.get(0), partitions.get(0), offsets.get(0), message);
+        //consumer.pause(Collections.singleton(new TopicPartition("kafka.topic", 0)));
 
 
-    }
+    }*/
 
-    @KafkaListener(id = "pause.resume",topics = "${app.topic.foo}",containerFactory = "kafkaListenerContainerFactory")
-    public void processMessage(String message,Consumer<?, ?> consumer,
+    @KafkaListener(id = "pause.resume", topics = "${app.topic.foo}", containerFactory = "kafkaListenerContainerFactory")
+    public void processMessage(String message, Consumer<?, ?> consumer,
                                @Header(KafkaHeaders.RECEIVED_PARTITION_ID) List<Integer> partitions,
                                @Header(KafkaHeaders.RECEIVED_TOPIC) List<String> topics,
                                @Header(KafkaHeaders.OFFSET) List<Long> offsets) {
         if (++recordCountPerIteration % 5 == 0) {
-            //System.out.printf("%s-%d[%d] \"%s\"\n", topics.get(0), partitions.get(0), offsets.get(0), message);
-            consumer.pause(Collections.singleton(new TopicPartition("kafka.topic", 0)));
-            //registry.getListenerContainer("pause.resume").pause();
+            System.out.println("Trying to pause container :" + dateFormat.format(new Date()));
+            System.out.println("Container state : "+registry.getListenerContainer("pause.resume").isPauseRequested());
+            registry.getListenerContainer("pause.resume").pause();
         }
         System.out.printf("%s-%d[%d] \"%s\"\n", topics.get(0), partitions.get(0), offsets.get(0), message);
     }
@@ -77,7 +95,7 @@ public class Receiver implements ApplicationListener<ListenerContainerIdleEvent>
             ConcurrentKafkaListenerContainerFactoryConfigurer configurer, ConsumerFactory<Object,Object> kafkaConsumerFactory){
         ConcurrentKafkaListenerContainerFactory<Object,Object> factory = new ConcurrentKafkaListenerContainerFactory();
         configurer.configure(factory, kafkaConsumerFactory);
-        factory.getContainerProperties().setIdleEventInterval(15_000L);
+        factory.getContainerProperties().setIdleEventInterval(15000L);
 
         return factory;
 
--- src/main/java/com/streaming/kafka/producer/Sender.java
+++ src/main/java/com/streaming/kafka/producer/Sender.java
@@ -1,19 +1,24 @@
 package com.streaming.kafka.producer;
 
 import com.streaming.kafka.LogManager1;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Service
 public class Sender {
 
     private static final LogManager1 logger = new LogManager1(Sender.class);
 
-   /* @Autowired
-    private KafkaTemplate<String, String> kafkaTemplate;*/
-
+   @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+   DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
     @Value("${app.topic.foo}")
     private String topic;
     int runNumber = 0;
@@ -22,7 +27,7 @@ public class Sender {
     public void runkafka() throws Exception{
 
         System.out.println("Starting runKafka method for runNumber: ["+runNumber+"]");
-
+        System.out.println("Sending messages to Topic : "+dateFormat.format( new Date()));
         for(int i=0;i<15;i++){
             send( runNumber+ ": Hello world:"+i);
 
@@ -35,11 +40,11 @@ public class Sender {
         kafkaTemplate.send(topic, message);
     }*/
 
-    private final KafkaTemplate<String, String> kafkaTemplate;
+   /* private final KafkaTemplate<String, String> kafkaTemplate;
 
     Sender(KafkaTemplate<String, String> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
-    }
+    }*/
 
     public void send(String message) {
         this.kafkaTemplate.send(topic, message);
